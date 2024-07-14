0d71e785da120e6c58fd650afdf574a70207dd03
Nicolas Karolak <nicolas@karolak.fr>
2024-07-14 15:23:06 +0200

    refactor(getsops): use bash script instead of thrid-party action

---
+++ getsops/action.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -o errexit  # abort on nonzero exitstatus
+set -o nounset  # abort on unbound variable
+set -o pipefail # don't hide errors within pipes
+
+# set version
+if [ "${INPUT_VERSION:-}" = "latest" ]; then
+	version=$(curl -fsSL https://api.github.com/repos/getsops/sops/releases | jq -r '.[0].name')
+else
+	version=$INPUT_VERSION
+fi
+version="${version/#v/}"
+
+# set os
+case $(uname) in
+Linux)
+	os=linux
+	;;
+Darwin)
+	os=darwin
+	;;
+*)
+	echo unsupported os
+	exit 1
+	;;
+esac
+
+# set architecture
+case $(uname -m) in
+arm64,aarch64)
+	arch=arm64
+	;;
+amd64,x86_64)
+	arch=amd64
+	;;
+*)
+	echo unsupported architecture
+	exit 1
+	;;
+esac
+
+# download release
+curl \
+	--location \
+	--remote-name \
+	"https://github.com/getsops/sops/releases/download/v${version}/sops-v${version}.${os}.${arch}"
+
+# check download
+curl \
+	--location \
+	--remote-name \
+	"https://github.com/getsops/sops/releases/download/v${version}/sops-v${version}.checksums.txt"
+case "$os" in
+linux)
+	sha256sum -c "sops-v$version.checksums.txt" --ignore-missing
+	;;
+darwin)
+	shasum --algorithm 256 --check --ignore-missing "sops-v$version.checksums.txt"
+	;;
+esac
+rm -f "sops-v$version.checksums.txt"
+
+# install
+install -m 0755 "sops-v${version}.${os}.${arch}" /usr/local/bin/sops
+rm -f "sops-v${version}.${os}.${arch}"
